a457a7c46df4abce8fc4993ae6fdffcd949d6952
Shammi Shailaj <shammishailaj@gmail.com>
2023-07-01 10:52:12 +0530

    - Added string types Exchange and TimeInterval and constants related to those - Added utility function to convert seconds into days - HistoryParams now uses valid relevant types to avoid confusion

--- history.go
+++ history.go
@@ -7,11 +7,11 @@ import (
 )
 
 type HistoryParams struct {
-	Exchange    string `json:"exchange"`
-	SymbolToken string `json:"symboltoken"`
-	Interval    string `json:"interval"`
-	FromDate    string `json:"fromdate"`
-	ToDate      string `json:"todate"`
+	Exchange    Exchange     `json:"exchange"`
+	SymbolToken string       `json:"symboltoken"`
+	Interval    TimeInterval `json:"interval"`
+	FromDate    time.Time    `json:"fromdate"`
+	ToDate      time.Time    `json:"todate"`
 }
 
 func (h *HistoryParams) GetParams() map[string]interface{} {
@@ -19,9 +19,70 @@ func (h *HistoryParams) GetParams() map[string]interface{} {
 	params["exchange"] = h.Exchange
 	params["symboltoken"] = h.SymbolToken
 	params["interval"] = h.Interval
-	params["fromdate"] = h.FromDate
-	params["todate"] = h.ToDate
+	params["fromdate"] = h.FromDate.Format(TimeFormatLayout)
+	params["todate"] = h.ToDate.Format(TimeFormatLayout)
 	return params
+}
+
+func (h *HistoryParams) ValidDates() bool {
+	if h.FromDate.Unix() < h.ToDate.Unix() {
+		return true
+	}
+	return false
+}
+
+func (h *HistoryParams) IntervalDays() int64 {
+	var days int64
+
+	if h.ValidDates() {
+		days = secondsToDays(h.ToDate.Unix() - h.FromDate.Unix())
+	} else {
+		days = secondsToDays(h.FromDate.Unix() - h.ToDate.Unix())
+	}
+
+	return days
+}
+
+func (h *HistoryParams) IsValidInterval() bool {
+	days := h.IntervalDays()
+
+	switch h.Interval {
+	case ONE_MINUTE:
+		if days <= 30 {
+			return true
+		}
+	case THREE_MINUTE:
+		if days <= 90 {
+			return true
+		}
+	case FIVE_MINUTE:
+		if days <= 90 {
+			return true
+		}
+	case TEN_MINUTE:
+		if days <= 90 {
+			return true
+		}
+	case FIFTEEN_MINUTE:
+		if days <= 180 {
+			return true
+		}
+	case THIRTY_MINUTE:
+		if days <= 180 {
+			return true
+		}
+	case ONE_HOUR:
+		if days <= 365 {
+			return true
+		}
+	case ONE_DAY:
+		if days <= 2000 {
+			return true
+		}
+	default:
+		return false
+	}
+	return false
 }
 
 type HistoryDatum struct {
@@ -49,8 +110,15 @@ func (h HistoryResponse) String() string {
 }
 
 // GetCandleData gets history of the specified symbol between a defined time-range
-func (c *Client) GetCandleData(params HistoryParams) (HistoryResponse, error) {
-	var history HistoryResponse
+func (c *Client) GetCandleData(params *HistoryParams) (*HistoryResponse, error) {
+	var history *HistoryResponse
+	if !params.ValidDates() {
+		return nil, fmt.Errorf("history.GetCandleData: fromdate can not be greater than todate")
+	}
+
+	if !params.IsValidInterval() {
+		return nil, fmt.Errorf("history.GetCandleData: interval days can not be %d when interval is %s. Please see %s for details", params.IntervalDays(), params.Interval, URLHistoryDocumentation)
+	}
 
 	err := c.doEnvelope(http.MethodGet, URIGetCandleData, params.GetParams(), nil, &history, true)
 	return history, err
--- utils.go
+++ utils.go
@@ -14,23 +14,63 @@ type Time struct {
 	time.Time
 }
 
+type Exchange string
+
+type TimeInterval string
+
+const (
+	// TimeFormatLayout for History API as described in the NOTE section of https://smartapi.angelbroking.com/docs/Historical
+	TimeFormatLayout string = "2006-01-02 15:04"
+
+	// NSE Exchange constant for NSE Equity as defined in documentation at: https://smartapi.angelbroking.com/docs/Historical
+	NSE Exchange = "NSE"
+
+	// NFO Exchange constant for Only available for NSE Futures as defined in documentation at: https://smartapi.angelbroking.com/docs/Historical
+	NFO Exchange = "NFO"
+
+	// ONE_MINUTE interval constant for "1 Minute" as defined in documentation at: https://smartapi.angelbroking.com/docs/Historical
+	ONE_MINUTE TimeInterval = "ONE_MINUTE"
+
+	// THREE_MINUTE interval constant for "3 Minute" as defined in documentation at: https://smartapi.angelbroking.com/docs/Historical
+	THREE_MINUTE TimeInterval = "THREE_MINUTE"
+
+	// FIVE_MINUTE interval constant for "5 Minute" as defined in documentation at: https://smartapi.angelbroking.com/docs/Historical
+	FIVE_MINUTE TimeInterval = "FIVE_MINUTE"
+
+	// TEN_MINUTE interval constant for "10 Minute" as defined in documentation at: https://smartapi.angelbroking.com/docs/Historical
+	TEN_MINUTE TimeInterval = "TEN_MINUTE"
+
+	// FIFTEEN_MINUTE interval constant for "15 Minute" as defined in documentation at: https://smartapi.angelbroking.com/docs/Historical
+	FIFTEEN_MINUTE TimeInterval = "FIFTEEN_MINUTE"
+
+	// THIRTY_MINUTE interval constant for "30 Minute" as defined in documentation at: https://smartapi.angelbroking.com/docs/Historical
+	THIRTY_MINUTE TimeInterval = "THIRTY_MINUTE"
+
+	// ONE_HOUR interval constant for "1 Hour" as defined in documentation at: https://smartapi.angelbroking.com/docs/Historical
+	ONE_HOUR TimeInterval = "ONE_HOUR"
+
+	// ONE_DAY interval constant for "1 Day" as defined in documentation at: https://smartapi.angelbroking.com/docs/Historical
+	ONE_DAY TimeInterval = "ONE_DAY"
+)
+
 // API endpoints
 const (
-	URILogin            string = "rest/auth/angelbroking/user/v1/loginByPassword"
-	URIUserSessionRenew string = "rest/auth/angelbroking/jwt/v1/generateTokens"
-	URIUserProfile      string = "rest/secure/angelbroking/user/v1/getProfile"
-	URILogout           string = "rest/secure/angelbroking/user/v1/logout"
-	URIGetOrderBook     string = "rest/secure/angelbroking/order/v1/getOrderBook"
-	URIPlaceOrder       string = "rest/secure/angelbroking/order/v1/placeOrder"
-	URIModifyOrder      string = "rest/secure/angelbroking/order/v1/modifyOrder"
-	URICancelOrder      string = "rest/secure/angelbroking/order/v1/cancelOrder"
-	URIGetHoldings      string = "rest/secure/angelbroking/portfolio/v1/getHolding"
-	URIGetPositions     string = "rest/secure/angelbroking/order/v1/getPosition"
-	URIGetTradeBook     string = "rest/secure/angelbroking/order/v1/getTradeBook"
-	URILTP              string = "rest/secure/angelbroking/order/v1/getLtpData"
-	URIRMS              string = "rest/secure/angelbroking/user/v1/getRMS"
-	URIConvertPosition  string = "rest/secure/angelbroking/order/v1/convertPosition"
-	URIGetCandleData    string = "rest/secure/angelbroking/historical/v1/getCandleData"
+	URILogin                string = "rest/auth/angelbroking/user/v1/loginByPassword"
+	URIUserSessionRenew     string = "rest/auth/angelbroking/jwt/v1/generateTokens"
+	URIUserProfile          string = "rest/secure/angelbroking/user/v1/getProfile"
+	URILogout               string = "rest/secure/angelbroking/user/v1/logout"
+	URIGetOrderBook         string = "rest/secure/angelbroking/order/v1/getOrderBook"
+	URIPlaceOrder           string = "rest/secure/angelbroking/order/v1/placeOrder"
+	URIModifyOrder          string = "rest/secure/angelbroking/order/v1/modifyOrder"
+	URICancelOrder          string = "rest/secure/angelbroking/order/v1/cancelOrder"
+	URIGetHoldings          string = "rest/secure/angelbroking/portfolio/v1/getHolding"
+	URIGetPositions         string = "rest/secure/angelbroking/order/v1/getPosition"
+	URIGetTradeBook         string = "rest/secure/angelbroking/order/v1/getTradeBook"
+	URILTP                  string = "rest/secure/angelbroking/order/v1/getLtpData"
+	URIRMS                  string = "rest/secure/angelbroking/user/v1/getRMS"
+	URIConvertPosition      string = "rest/secure/angelbroking/order/v1/convertPosition"
+	URIGetCandleData        string = "rest/secure/angelbroking/historical/v1/getCandleData"
+	URLHistoryDocumentation string = "https://smartapi.angelbroking.com/docs/Historical"
 )
 
 func structToMap(obj interface{}, tagName string) map[string]interface{} {
@@ -173,4 +213,8 @@ func getPublicIp() (string, error) {
 		return "", err
 	}
 	return string(content), nil
+}
+
+func secondsToDays(seconds int64) int64 {
+	return int64(((seconds / 60) / 60) / 24)
 }
